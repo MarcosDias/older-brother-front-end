030536e78d0c4d2f758eb0b3e8d160c898246430
Marcos Dias <marcos.a.melo.dias@gmail.com>
2017-02-06 22:34:26 -0200

    Input de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
-import {Menu} from 'semantic-ui-react';
+import {Menu, Grid, Container, Header, Input, Icon} from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Menu className="five item fixed inverted">
-          <div className="ui container">
-            <Menu.Item name="Produtos" active={true} link={true}/>
+        <Menu className="five item inverted" stackable>
+          <Container text>
+            <Menu.Item name="Produtos" active link={true}/>
             <Menu.Item name="Lojas" link={true}/>
             <Menu.Item name="Older Brother" header={true} link={true} />
             <Menu.Item name="Entrar" link={true}/>
             <Menu.Item name="Carrinho" link={true}/>
-          </div>
-        </Menu >
+          </Container>
+        </Menu>
+        <Grid centered>
+          <Grid.Row>
+            <Grid.Column width={13}>
+              <Input fluid
+                icon={<Icon name='search' inverted circular link />}
+                placeholder='Pesquisar...'
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Grid text className="main text">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              adfasdfas
+            </Grid.Column>
+            <Grid.Column width={13}>
+              adfasdfas
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
